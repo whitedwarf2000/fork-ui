1bd351bd399f08dd5244afe150889b64ed3d0f73
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-21 15:58:42 +0700

    Fix Confirm

--- src/components/PureConfirm/PureConfirm.js
+++ src/components/PureConfirm/PureConfirm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
+import Button from '../Button';
+
 require('./PureConfirm.scss');
 
 const PureConfirm = ({
@@ -20,8 +22,8 @@ const PureConfirm = ({
         <div className="rc-pure-confirm-content">{children}</div>
       </div>
       <div className="rc-pure-confirm-buttons">
-        <span className="rc-pure-confirm-cancel" onClick={onCancelClick}>Cancel</span>
-        <span className="rc-pure-confirm-ok" onClick={onOkClick}>OK</span>
+        <Button className="rc-pure-confirm-cancel" onClick={onCancelClick}>Cancel</Button>
+        <Button className="rc-pure-confirm-ok" onClick={onOkClick}>OK</Button>
       </div>
     </div>
   );
