587f8ce3d726ccebe9a4cf32986215d55f4281fb
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-24 12:52:03 +0700

    Fix UI system

--- src/pages/documents/Breadcrumb.box.js
+++ src/pages/documents/Breadcrumb.box.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import cn from 'classnames';
-import { Breadcrumb } from '../../components/core';
+import { Breadcrumb, Icon } from '../../components/core';
 import PracticeBox from '../../components/PracticeBox';
 
 export default ({ className }) => {
   return (
     <PracticeBox wrapperClassName={className} className="flex items-center" header="BREADCRUM">
       <Breadcrumb>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>Components</Breadcrumb.Item>
+        <Breadcrumb.Item><Icon name="home" /></Breadcrumb.Item>
         <Breadcrumb.Item active>Breadcrumb</Breadcrumb.Item>
       </Breadcrumb>
     </PracticeBox>
--- src/pages/documents/Tooltip.box.js
+++ src/pages/documents/Tooltip.box.js
@@ -10,67 +10,67 @@ export default ({ className }) => {
     <PracticeBox wrapperClassName={className} className="flex items-center" header="TOOLTIP">
       <div className="flex flex-col w-full items-center">
         <div className="flex">
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
           <Tooltip placement="top-left" label="Top-Left tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>TL</Button>
+            <Button className="m-2" style={{ width: '3em' }}>TL</Button>
           </Tooltip>
           <Tooltip placement="top" label="Top tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>Top</Button>
+            <Button className="m-2" style={{ width: '3em' }}>T</Button>
           </Tooltip>
           <Tooltip placement="top-right" label="Top-Right tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>TR</Button>
+            <Button className="m-2" style={{ width: '3em' }}>TR</Button>
           </Tooltip>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
         </div>
 
         <div className="flex">
           <Tooltip placement="left-top" label="Left-Top tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>LT</Button>
+            <Button className="m-2" style={{ width: '3em' }}>LT</Button>
           </Tooltip>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
           <Tooltip placement="right-top" label="Right-To tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>RT</Button>
+            <Button className="m-2" style={{ width: '3em' }}>RT</Button>
           </Tooltip>
         </div>
 
         <div className="flex">
           <Tooltip placement="left" label="Left tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>Left</Button>
+            <Button className="m-2" style={{ width: '3em' }}>L</Button>
           </Tooltip>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
           <Tooltip placement="right" label="Right tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>Right</Button>
+            <Button className="m-2" style={{ width: '3em' }}>R</Button>
           </Tooltip>
         </div>
 
         <div className="flex">
           <Tooltip placement="left-bottom" label="Left-Bottom tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>LB</Button>
+            <Button className="m-2" style={{ width: '3em' }}>LB</Button>
           </Tooltip>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
           <Tooltip placement="right-bottom" label="Right-Bottom tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>RB</Button>
+            <Button className="m-2" style={{ width: '3em' }}>RB</Button>
           </Tooltip>
         </div>
 
         <div className="flex">
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
           <Tooltip placement="bottom-left" label="Bottom-Left tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>BL</Button>
+            <Button className="m-2" style={{ width: '3em' }}>BL</Button>
           </Tooltip>
           <Tooltip placement="bottom" label="Bottom tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>Bottom</Button>
+            <Button className="m-2" style={{ width: '3em' }}>V</Button>
           </Tooltip>
           <Tooltip placement="bottom-right" label="Bottom-Right tooltip">
-            <Button className="m-2" style={{ width: '7em' }}>BR</Button>
+            <Button className="m-2" style={{ width: '3em' }}>BR</Button>
           </Tooltip>
-          <Button transparent disabled className="m-2" style={{ width: '7em' }}></Button>
+          <Button transparent disabled className="m-2" style={{ width: '3em' }}></Button>
         </div>
       </div>
     </PracticeBox>
