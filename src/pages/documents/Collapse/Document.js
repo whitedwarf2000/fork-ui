80fadc918d73f4821791276e32ed8c7a5489683d
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-29 19:00:41 +0700

    Fix layout

--- src/pages/documents/Collapse/Document.js
+++ src/pages/documents/Collapse/Document.js
@@ -11,25 +11,23 @@ import OnChangeEvent from './demo/OnChangeEvent';
 export default () => {
   return (
     <div className="flex">
-      <div className="w-1/3 pr-2">
+      <div className="w-1/2 pr-2">
         <Codebox className="mb-5" header="BASIC" href="collapse-basic" code={Basic.code}>
           <Basic />
         </Codebox>
         <Codebox className="mb-5" header="DISABLED" href="collapse-disabled" code={Disabled.code}>
           <Disabled />
         </Codebox>
+        <Codebox className="mb-5" header="CONTROLLED" href="collapse-controlled" code={Controlled.code}>
+          <Controlled />
+        </Codebox>
       </div>
-      <div className="w-1/3 pl-2">
+      <div className="w-1/2 pl-2">
         <Codebox className="mb-5" header="ACCORDION" href="collapse-accordion" code={Accordion.code}>
           <Accordion />
         </Codebox>
         <Codebox className="mb-5" header="WITH ICON" href="collapse-icon" code={IconCustom.code}>
           <IconCustom />
-        </Codebox>
-      </div>
-      <div className="w-1/3 pl-2">
-        <Codebox className="mb-5" header="CONTROLLED" href="collapse-controlled" code={Controlled.code}>
-          <Controlled />
         </Codebox>
         <Codebox className="mb-5" header="ON PANELS CHANGE EVENT" href="collapse-change-event" code={OnChangeEvent.code}>
           <OnChangeEvent />
