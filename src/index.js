7b5fc47cdb51d0fdf0ae2ca0fa116cb7727ad8f2
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-04-02 00:48:43 +0700

    Relase first version v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Workbox } from 'workbox-window';
-import App from './App';
-import pushNotification from './components/Notification/push';
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-if ('serviceWorker' in navigator) {
-  const wb = new Workbox('/service-worker.js');
-
-  wb.register();
-}
+exports.default = require('./commonjs');
