77bd86816ea0715c5082eeab7d034ead27213144
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-11-01 16:55:47 +0700

    update switch

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
+
 import Loader from '../Loader';
 
-const Switch = React.forwardRef(({ checked, className, disabled, loading, ...otherProps }, ref) => {
+const Switch = React.forwardRef(({ className, disabled, loading, ...otherProps }, ref) => {
   return (
-    <span className={cn('fswitch', { 'fswitch-checked': checked, 'fswitch-disabled': disabled }, className)}>
+    <div className={cn('fswitch', className)}>
       <input
         type="checkbox"
         className="fswitch-input"
         ref={ref}
-        checked={checked}
         disabled={disabled || loading}
         {...otherProps}
       />
-      <span className="fswitch-mark">
-        {loading && <Loader.Spinner />}
-      </span>
-    </span>
+      <div className="fswitch-fake">
+        <span className="fswitch-mark">
+          {loading && <Loader.Spinner />}
+        </span>
+      </div>
+    </div>
   );
 });
 
 Switch.displayName = 'Switch';
 Switch.propTypes = {
   className: PropTypes.string,
-  checked: PropTypes.bool,
   disabled: PropTypes.bool,
   loading: PropTypes.bool,
 };
-Switch.defaultProps = {
-  onChange: f => f,
-};
+Switch.defaultProps = {};
 
 export default Switch;
