0aebe5c1274facbcc8214a2a89fa1406237071ea
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-11-03 23:27:57 +0700

    update typo

--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -5,12 +5,12 @@ import useSemanticProp from '../../hooks/useSemanticProp';
 import { omit } from '../../utils/helpers';
 
 const mFontStyles = Object.freeze({
-  italic: 'ftypo-italic',
+  i: 'ftypo-italic',
   oblique: 'ftypo-oblique',
 });
 
 const mTextDecoration = Object.freeze({
-  underline: 'ftypo-underline',
+  u: 'ftypo-underline',
   through: 'ftypo-line-through',
 });
 
@@ -18,7 +18,7 @@ const lTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'p', 'span'];
 const lFontStyles = Object.keys(mFontStyles);
 const lTextDecoration = Object.keys(mTextDecoration);
 
-const Typography = ({ children, className, disabled, color, strong, ...otherProps }) => {
+const Typography = ({ children, className, disabled, color, b, ...otherProps }) => {
   const tag = useSemanticProp('tag', otherProps, lTags) || 'p';
   const fontStyle = useSemanticProp('fontStyle', otherProps, lFontStyles);
   const textDecoration = useSemanticProp('decoration', otherProps, lTextDecoration);
@@ -42,7 +42,7 @@ const Typography = ({ children, className, disabled, color, strong, ...otherProp
         'ftypo',
         {
           'ftypo-disabled': disabled,
-          'ftypo-strong': strong,
+          'ftypo-strong': b,
         },
         mFontStyles[fontStyle],
         mTextDecoration[textDecoration],
@@ -60,11 +60,11 @@ Typography.propTypes = {
   className: PropTypes.string,
   disabled: PropTypes.bool,
   decoration: PropTypes.string,
-  underline: PropTypes.bool,
+  u: PropTypes.bool,
   through: PropTypes.bool,
-  strong: PropTypes.bool,
+  b: PropTypes.bool,
   fontStyle: PropTypes.string,
-  italic: PropTypes.bool,
+  i: PropTypes.bool,
   p: PropTypes.bool,
   tag: PropTypes.string,
   div: PropTypes.bool,
