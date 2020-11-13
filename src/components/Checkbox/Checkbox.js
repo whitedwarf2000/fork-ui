85ba74df81979dec26429c6e630955ddbdfe3103
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-11-13 21:24:35 +0700

    update checkbbox

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -11,7 +11,12 @@ const Checkbox = React.forwardRef(({ className, style, ...otherProps }, ref) => 
         ref={ref}
         {...otherProps}
       />
-      <div className="fcbox-mark" />
+      <div className="fcbox-mark">
+        <svg className="fcbox-check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M5 12l5 5l10 -10" />
+        </svg>
+      </div>
     </span>
   );
 });
