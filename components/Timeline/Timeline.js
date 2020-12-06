a5585a6593ed915a53b4d47e7495703b4df15a5a
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-12-06 19:31:41 +0700

    Update timeline

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -1,24 +1,78 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
+import { Clock } from '../icons';
 
-import Item from './Item';
+import useSemanticProp from '../../hooks/useSemanticProp';
+import { omit } from '../../utils/helpers';
 
-const Timeline = ({ className, children, right, reverse, ...otherProps }) => {
+const Connector = ({ className, ...otherProps}) => <div className={cn('ftimeline-connector', className)} {...otherProps} />;
+const Content = ({ className, ...otherProps}) => <div className={cn('ftimeline-content', className)} {...otherProps} />;
+const Dot = ({ className, ...otherProps}) => <div className={cn('ftimeline-dot', className)} {...otherProps} />;
+const Item = ({ className, ...otherProps }) => <li className={cn('ftimeline-item', className)} {...otherProps} />;
+
+const mAlign = Object.freeze({
+  right: 'ftimeline-r',
+  left: 'ftimeline-l',
+  alternate: 'ftimeline-alternate',
+});
+
+const lAlign = Object.keys(mAlign);
+
+const Timeline = ({ className, reverse, ...otherProps }) => {
+  const align = useSemanticProp('align', otherProps, lAlign);
+
+  // ignore semantic props
+  const passedProps = useMemo(() => omit(otherProps, [
+    ...lAlign,
+    'align',
+  ]), [otherProps]);
+
   return (
-    <div className={cn('ftimeline', { 'ftimeline-r': right, 'ftimeline-reverse': reverse }, className)} {...otherProps}>
-      {children}
-    </div>
+    <ul
+      className={cn(
+        'ftimeline',
+        mAlign[align],
+        {
+          'ftimeline-reverse': reverse
+        },
+        className
+      )}
+      {...passedProps}
+    />
   );
 }
 
-Timeline.Item = Item;
-
 Timeline.displayName = 'Timeline';
+Connector.displayName = 'Timeline.Connector';
+Content.displayName = 'Timeline.Content';
+Dot.displayName = 'Timeline.Dot';
+Item.displayName = 'Timeline.Item';
+
+Connector.defaultProps = {
+  className: PropTypes.string,
+};
+Content.defaultProps = {
+  className: PropTypes.string,
+};
+Dot.defaultProps = {
+  children: <Clock />,
+  className: PropTypes.string,
+};
+Item.defaultProps = {
+  className: PropTypes.string,
+};
+
+Timeline.Item = Item;
+Timeline.Connector = Connector;
+Timeline.Content = Content;
+Timeline.Dot = Dot;
+
 Timeline.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.any,
   right: PropTypes.bool,
+  left: PropTypes.bool,
+  alternate: PropTypes.bool,
   reverse: PropTypes.bool,
 };
 Timeline.defaultProps = {};
